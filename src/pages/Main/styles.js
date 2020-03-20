8dd4bf1d11af0318e5b95bd952f67dc12a2105fc
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-20 15:01:30 -0400

    Created functionality to list added repositories

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -75,3 +75,25 @@ export const SubmitButton = styled.button.attrs(props => ({
       }
     `}
 `;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px 0;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
+`;
