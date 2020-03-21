886710b2e7c2484374755a1ff68db73907fb0982
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-21 19:13:12 -0400

    Styled issues list

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList, Label } from './styles';
 
 class Repository extends Component {
   static propTypes = {
@@ -58,6 +58,25 @@ class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <Label key={String(label.id)} color={label.color}>
+                      {label.name}
+                    </Label>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
